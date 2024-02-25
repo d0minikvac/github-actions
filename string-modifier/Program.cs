8d8a8675ce7d332ae2fdf37c9f86450b6507fbe8
d0minikvac <dominikbirovac@gmail.com>
2024-02-25 23:17:33 +0100

    Project created

---
+++ string-modifier/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace string_modifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
