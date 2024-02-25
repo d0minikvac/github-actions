0b28030ae655e1b25f76b61382994c3a4f048c44
d0minikvac <dominikbirovac@gmail.com>
2024-02-25 23:37:13 +0100

    CutFirstLetter function

--- string-modifier/Program.cs
+++ string-modifier/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace string_modifier
 {
-    internal class Program
+    public class Program
     {
+        public static string CutFirstLetter(string input)
+        {
+            string newString = "";
+            for (int i = 1; i < input.Length; i++)
+            {
+                newString += input[i];
+            }
+            return newString;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Feladat");
+            Console.WriteLine(CutFirstLetter("Random"));
         }
     }
 }
