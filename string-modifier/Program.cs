1a830e2364382551e7fbbe0ec46a7621287889ab
d0minikvac <dominikbirovac@gmail.com>
2024-02-26 13:20:14 +0100

    Tests added

--- string-modifier/Program.cs
+++ string-modifier/Program.cs
@@ -2,30 +2,16 @@
 {
     public class Program
     {
-        public static string CutFirstLetter(string input)
-        {
-            string newString = "";
-            for (int i = 1; i < input.Length; i++)
-            {
-                newString += input[i];
-            }
-            return newString;
-        }
-
-        public static string CutLastLetter(string input)
-        {
-            string newString = "";
-            for (int i = 0; i < input.Length-1; i++)
-            {
-                newString += input[i];
-            }
-            return newString;
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine(CutFirstLetter("Elso"));
-            Console.WriteLine(CutLastLetter("Utolso"));
+            StringModifier sm = new StringModifier();
+            string input = "Hello";
+
+            string result = sm.CutFirstLetter(input);
+            Console.WriteLine(result);
+
+            string result2 = sm.CutLastLetter(input);
+            Console.WriteLine(result2);
         }
     }
 }
